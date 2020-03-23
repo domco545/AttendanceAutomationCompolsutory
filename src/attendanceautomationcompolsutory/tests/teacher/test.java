bd0c40e344997e5911b509eb6539038dc62c7290
ArmNem <58069062+ArmNem@users.noreply.github.com>
2020-03-23 21:07:21 +0100

    f

---
+++ src/attendanceautomationcompolsutory/tests/teacher/test.java
@@ -0,0 +1,55 @@
+package attendanceautomationcompolsutory.tests.teacher;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import attendanceautomationcompolsutory.dal.TeacherDB;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author narma
+ */
+public class test {
+    
+    public test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+     @Test
+    public void testGetTeacherMail()
+    {
+        TeacherDB teacher = new TeacherDB();
+        assertEquals("teacher@easv.dk",teacher.getTeacherMail(42));
+    }
+}
