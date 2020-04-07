5e75b54cdf02d55e631a166a361a2cda77603046
Rezas84 <54980966+Rezas84@users.noreply.github.com>
2020-04-07 21:09:27 +0200

    current date label

--- src/attendanceautomationcompolsutory/gui/controller/StudentMainController.java
+++ src/attendanceautomationcompolsutory/gui/controller/StudentMainController.java
@@ -6,17 +6,28 @@
 package attendanceautomationcompolsutory.gui.controller;
 
 import attendanceautomationcompolsutory.be.LoggedUser;
+import attendanceautomationcompolsutory.be.Subject;
+import attendanceautomationcompolsutory.dal.SubjectDB;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXToggleButton;
 import java.io.IOException;
 import java.net.NetworkInterface;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -25,9 +36,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -69,6 +82,8 @@ public class StudentMainController implements Initializable {
         lblEmail.setText(user.email);
         imgViewProfile.setImage(user.image);
         checkVPN();
+        getCurrentDate();
+        initSubjectLabel();
     }
 
     @FXML
@@ -143,17 +158,35 @@ public class StudentMainController implements Initializable {
                 if (networkInterface.isUp()) {
                     String iName = networkInterface.getName();
                     System.out.println(iName);
-                    if(iName.contains("tun") || iName.contains("ppp") || iName.contains("pptp")){
+                    if (iName.contains("tun") || iName.contains("ppp") || iName.contains("pptp")) {
                         System.out.println("VPN connection found");
                         found = true;
                     }
                 }
             }
-            if(found == false){
+            if (found == false) {
                 System.out.println("VPN connection not found");
             }
         } catch (Exception ex) {
             System.out.println("No interfaces found");
         }
+    }
+
+    private void getCurrentDate() {
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            Calendar time = Calendar.getInstance();
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+            lblDate.setText(simpleDateFormat.format(time.getTime()));
+        }),
+                new KeyFrame(Duration.seconds(1))
+        );
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
+
+    private void initSubjectLabel() {
+        SubjectDB subjectDB = new SubjectDB();
+        List<Subject> subjects = subjectDB.getAllSubject();
+       
     }
 }
