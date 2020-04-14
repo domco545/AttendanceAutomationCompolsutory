80f90294296a722cce5d546273853c54a49f14b5
Dominik Rothbauer <dominikrothbauer@gmail.com>
2020-04-14 18:44:34 +0200

    teacher main gui changed

--- src/attendanceautomationcompolsutory/gui/controller/TeacherMainController.java
+++ src/attendanceautomationcompolsutory/gui/controller/TeacherMainController.java
@@ -5,30 +5,13 @@
  */
 package attendanceautomationcompolsutory.gui.controller;
 
-import attendanceautomationcompolsutory.be.Student;
-import attendanceautomationcompolsutory.bll.TeacherManager;
-import attendanceautomationcompolsutory.dal.ITeacherDB;
+import attendanceautomationcompolsutory.be.LoggedUser;
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTreeTableColumn;
-
-import com.jfoenix.controls.JFXTreeTableView;
-import com.jfoenix.controls.RecursiveTreeItem;
-import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import java.io.IOException;
-import java.net.NetworkInterface;
-import java.net.SocketException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -37,11 +20,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.SplitMenuButton;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableColumn;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
 /**
  * FXML Controller class
@@ -50,24 +30,10 @@ import javafx.util.Callback;
  */
 public class TeacherMainController implements Initializable {
 
-    //  @FXML
-    //  private JFXTreeTableView<Student> tableview;
-    @FXML
-    private TreeTableColumn<Student, Integer> id;
-
-    @FXML
-    private TreeTableColumn<Student, String> fname;
-    @FXML
-    private TreeTableColumn<Student, String> lname;
-    ITeacherDB Iteacher = new TeacherManager();
-
-    private ObservableList<Student> obsStudents = FXCollections.observableArrayList(Iteacher.getStudentData());
     @FXML
     private JFXButton BtnDetail;
     @FXML
     private Label LblCurrentDay;
-    @FXML
-    private SplitMenuButton selectClassDropdown;
     @FXML
     private Label LblEmail;
     @FXML
@@ -76,39 +42,19 @@ public class TeacherMainController implements Initializable {
     private JFXButton BtnClassOverview;
     @FXML
     private JFXButton BtnLogout;
-
-    Student student;
+    @FXML
+    private Label lblClass;
+    @FXML
+    private ImageView imgProfile;
+    
+    LoggedUser user;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
-        //
-//         id  = new JFXTreeTableColumn<>("id");
-//        fname = new JFXTreeTableColumn<>("fname");
-//        lname = new JFXTreeTableColumn<>("lname");
-//
-//        fname.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Student, String>, ObservableValue<String>>() {
-//            @Override
-//            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Student, String> param) {
-//                return param.getValue().getValue().nameProperty();
-//
-//            }
-//        });
-//        lname.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Student, String>, ObservableValue<String>>() {
-//            @Override
-//            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Student, String> param) {
-//                return param.getValue().getValue().lnameProperty();
-//
-//            }
-//        });
-//
-//        TreeItem<Student> root = new RecursiveTreeItem<Student>(obsStudents, RecursiveTreeObject::getChildren);
-//        tableview.getColumns().setAll(fname, lname);
-//        tableview.setRoot(root);
-//        tableview.setShowRoot(false);
-
-        LblEmail.setText(Iteacher.getTeacherMail(student.getId()));
-        LblName.setText(Iteacher.getTeacherName(student.getId()));
+        user = LoggedUser.getInstance();
+        LblName.setText(user.fNmae + " " + user.lName);
+        LblEmail.setText(user.email);
+        imgProfile.setImage(user.image);
     }
 
     @FXML
@@ -168,7 +114,7 @@ public class TeacherMainController implements Initializable {
         } catch (IOException ex) {
             Logger.getLogger(TeacherMainController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
 
 }
