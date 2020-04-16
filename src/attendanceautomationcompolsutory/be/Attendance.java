1ee4877e73d9f373923f068432c1ae1cbfb58a9b
Rezas84 <54980966+Rezas84@users.noreply.github.com>
2020-04-16 14:18:55 +0200

    Table can show date by month only
    Need to chabge something yet and need to add the week and the semester too.

---
+++ src/attendanceautomationcompolsutory/be/Attendance.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendanceautomationcompolsutory.be;
+
+import java.util.Date;
+
+/**
+ *
+ * @author saraf
+ */
+public class Attendance {
+    private Date date;
+    private String state;
+
+    public Attendance(Date date, String state) {
+        this.date = date;
+        this.state = state;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+    
+}
---
+++ src/attendanceautomationcompolsutory/dal/AttendanceDB.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendanceautomationcompolsutory.dal;
+
+import attendanceautomationcompolsutory.be.Attendance;
+import attendanceautomationcompolsutory.be.LoggedUser;
+import attendanceautomationcompolsutory.be.Subject;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author saraf
+ */
+public class AttendanceDB {
+
+    DBConnection db = new DBConnection();
+
+    public ArrayList<Attendance> getAttendanceByFilter(String subjectName, String periodType, int periodValue) {
+        int studentId = LoggedUser.getInstance().id;
+        System.out.println(studentId);
+        if ("Week".equals(periodType)) {
+            //TODO
+        } else if ("Month".equals(periodType)) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(2020, periodValue-1, 1);
+            Date monthStart = calendar.getTime();
+            calendar.add(Calendar.MONTH, 1);
+            calendar.add(Calendar.DAY_OF_MONTH, -1);
+            Date monthEnd = calendar.getTime();
+            try ( Connection con = db.getConnection()) {
+                String sql = "SELECT * FROM Attendance Where student_id = ? AND date >= ? AND date <= ?  ";
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setInt(1, studentId);
+                ps.setDate(2, new java.sql.Date(monthStart.getTime()));
+                ps.setDate(3, new java.sql.Date(monthEnd.getTime()));
+                ResultSet rs = ps.executeQuery();
+                ArrayList<Attendance> attendances = new ArrayList<>();
+                while (rs.next()) {
+                    java.sql.Date date = rs.getDate("date");
+                    attendances.add(new Attendance(date, "att"));
+                }
+                return attendances;
+            } catch (SQLServerException ex) {
+                Logger.getLogger(TeacherDB.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(TeacherDB.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return null;
+    }
+}
